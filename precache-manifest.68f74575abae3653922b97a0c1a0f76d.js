66f6f3749f67c1a1d373eeda058043926a6940d5
Alexander Rao <alexhrao@gmail.com>
2020-02-16 12:09:01 -0500

    Updates

---
+++ precache-manifest.68f74575abae3653922b97a0c1a0f76d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19d9872e91a7c236f72787a53ae6a064",
+    "url": "/food-truck-classifier/index.html"
+  },
+  {
+    "revision": "00b09a69a0c0fac2bc0e",
+    "url": "/food-truck-classifier/static/css/main.eb7cb253.chunk.css"
+  },
+  {
+    "revision": "e10ded9664fab844266c",
+    "url": "/food-truck-classifier/static/js/2.494105e9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/food-truck-classifier/static/js/2.494105e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00b09a69a0c0fac2bc0e",
+    "url": "/food-truck-classifier/static/js/main.0529459d.chunk.js"
+  },
+  {
+    "revision": "a0ab449e577f121f1705",
+    "url": "/food-truck-classifier/static/js/runtime-main.c7f81410.js"
+  }
+]);
